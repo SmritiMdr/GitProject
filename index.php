61485df9414ce0e639258496ba456d31089c0d52
SmritiMdr <SmritiMdr@users.noreply.github.com>
2020-10-14 13:47:53 +0545

    adding index.php and style.css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+    <TITLE> GIT HUB </TITLE>
+    <link rel="stylesheet" href="style.css" />
+</HEAD>
+<BODY>
+    <H1> GITHUB TUTORIAL </H1>
+    <H2> Working with GITBASH</H2>
+    <H3> CONNECTING TO GITHUB</H3> 
+</BODY>
+</HTML>
+
